00996efadbdb374693195b97e261d69419f21e90
ctrl-fmadera <fmadera@gbox.adnu.edu.ph>
2025-10-07 01:28:14 +0800

    PROJ-1

---
+++ models/Project.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+}, { timestamps: true })
+
+module.exports = mongoose.model('Task', taskSchema)
